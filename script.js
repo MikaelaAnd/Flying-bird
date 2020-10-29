7de42c50642629a2f8365dff2176bbc0780c9bde
Mikaela Andersson <mikaelaaand@gmail.com>
2020-10-29 09:53:43 +0100

    added function fly

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Event som triggas, inga paranteser vilket säger att funktionen ska köras när det är dags, inte direkt.
-window.onload = start();
-
+window.onload = start;
+let left = 0;
 
 function start() {
-    const img = document.querySelector('img');
+    fly();
+    
 }
+// Denna funktion kmr köras en gång i sekunden, vilket gör att fågeln flyttar på sig
+function fly() {
+    setInterval(moveBirdToTheRight, 12);
+    
+}
+
+function moveBirdToTheRight() {
+    //Fetch bird from html
+    const bird = document.querySelector('img');
+    // Update position
+    left += 0.1;
+    //Render position
+    bird.style.left = left + '%';
+}
+
