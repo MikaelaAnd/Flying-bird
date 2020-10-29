420b39b988128cee4a29a4c0fd3e5293898a1f30
Mikaela Andersson <mikaelaaand@gmail.com>
2020-10-29 09:33:10 +0100

    added start function

---
+++ script.js
@@ -0,0 +1,7 @@
+// Event som triggas, inga paranteser vilket säger att funktionen ska köras när det är dags, inte direkt.
+window.onload = start();
+
+
+function start() {
+    const img = document.querySelector('img');
+}
