6bd2d1c35d1631c59c85a5bfd973586e580acfe3
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-17 10:42:33 +0100

    made bird move back to the left again

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ window.onload = start;
 /**The position of the bird relative to the left side of the page*/
 let left = 0;
 
+
+
 /** Starts the program on page load*/
 function start() {
-    fly();
-    
+    fly(); 
 }
+
 // Denna funktion kmr köras en gång i sekunden, vilket gör att fågeln flyttar på sig
 /** Sets interval to make bird "fly" */
 function fly() {
     setInterval(moveBirdToTheRight, 12);
     //fågeln flyttar till vänster efter 4sek.
-    // setTimeout(moveBirdToTheLeft, 4000)
+    
+    
 }
 /**  Moves the bird small steps in percentage to the right*/
 function moveBirdToTheRight() {
@@ -24,5 +27,16 @@ function moveBirdToTheRight() {
     left += 0.1;
     //Render position
     bird.style.left = left + '%';
+    if (left > 30) {
+        setInterval(moveBirdToTheLeft, 12);
+        left -= 0.1;
+    }
 }
 
+function moveBirdToTheLeft() {
+    left -= 0.1;
+    if (left < 0) {
+        setInterval(moveBirdToTheRight, 12);
+        left += 0.1;
+    }
+}
